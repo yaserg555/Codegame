5ba6aa04c8f9f17080a3adb668a396adf2f4d2a8
yaserg555 <yaserg555@gmail.com>
2015-09-21 00:43:46 +0300

    282 after minimize.

---
+++ src/com/codegame/test/Optimizations/CodeSize/Player.java
@@ -0,0 +1,19 @@
+package com.codegame.test.Optimizations.CodeSize;
+
+import com.sun.org.apache.bcel.internal.generic.GOTO;
+
+import java.util.*;
+
+class Player {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int o = in.nextInt(), t = in.nextInt(), k = in.nextInt(), l = in.nextInt();
+
+        while (true) {
+
+            System.out.println((l > t ? "N" : l < t ? "S" : "") + (k > o ? "W" : k < o ? "E" : ""));
+            k += k > o ? -1 : k < o ? 1 : 0;
+            l += l > t ? -1 : l < t ? 1 : 0;
+        }
+    }
+}
